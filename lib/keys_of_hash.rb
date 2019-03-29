df6f986c079fccc6caf0111669be7e4ae31330d9
Jason Bamford <47010074+jb-23@users.noreply.github.com>
2019-03-29 21:42:40 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arg)
+    self.collect do | key, val |
+      flag = false
+      arg.each do | arg_n |
+        flag = true if val == arg_n
+      end
+      flag ? key : nil
+    end.compact
   end
 end
